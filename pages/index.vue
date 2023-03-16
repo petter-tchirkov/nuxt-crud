676746a598b4cf91f22e21c96480471759e2fcc5
Petro chirkov <petro.chirkov@sevoyer.com>
2023-03-16 17:27:36 +0200

    store to store setup()

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-    <div class="text-5xl">huy</div>
+    <div class="posts p-40 grid grid-cols-4 gap-4">
+        <NuxtLink
+            to="/post"
+            v-for="post in postsStore.posts"
+            :key="post.id">
+            <div
+                class="post__item flex flex-col gap-3 p-2 bg-gray-200 hover:bg-gray-300 border border-cyan-400 hover:border-cyan-800 transition-all rounded-xl cursor-pointer">
+                <span>{{ post.id }}</span>
+                <h2 class="post__title text-3xl text-center">{{
+                    post.title
+                }}</h2>
+                <p class="post__body text-center">{{ post.body }}</p>
+            </div>
+        </NuxtLink>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { usePostsStore } from '~~/store/posts'
+const postsStore = usePostsStore()
+
+await postsStore.fetchPosts()
+</script>
 
 <style scoped></style>
---
+++ store/posts.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const usePostsStore = defineStore('users', () => {
+    let posts = ref([] as Record<string, any>[])
+    const fetchPosts = async () => {
+        const { data }: any = await useFetch(
+            'https://jsonplaceholder.typicode.com/posts'
+        )
+        if (data.value) {
+            posts.value = data.value
+        }
+    }
+
+    return { posts, fetchPosts }
+})
