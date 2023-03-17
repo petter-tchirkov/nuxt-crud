c528eb2f8a4b4050e64aa3a2f078f8759ec5eeb6
Petro chirkov <petro.chirkov@sevoyer.com>
2023-03-17 09:07:59 +0200

    post-routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="posts p-40 grid grid-cols-4 gap-4">
-        <NuxtLink
-            :to="`/posts/${post.id}`"
-            :params="{ title: post.title, id: post.id, body: post.body }"
-            v-for="post in postsStore.posts"
-            :key="post.id">
-            <div
-                class="post__item flex flex-col gap-3 p-2 bg-gray-200 hover:bg-gray-300 border border-cyan-400 hover:border-cyan-800 transition-all rounded-xl cursor-pointer">
-                <span>{{ post.id }}</span>
-                <h2 class="post__title text-3xl text-center">{{
-                    post.title
-                }}</h2>
-                <p class="post__body text-center">{{ post.body }}</p>
-            </div>
-        </NuxtLink>
+    <div>
+        <div class="post__create">
+            <NuxtLink to="/posts/create">Create Post</NuxtLink>
+        </div>
+        <div class="posts p-40 grid grid-cols-4 gap-4">
+            <NuxtLink
+                :to="`/posts/${post.id}`"
+                :params="{ title: post.title, id: post.id, body: post.body }"
+                v-for="post in postsStore.posts"
+                :key="post.id">
+                <div
+                    class="post__item flex flex-col gap-3 p-2 bg-gray-200 hover:bg-gray-300 border border-cyan-400 hover:border-cyan-800 transition-all rounded-xl cursor-pointer">
+                    <span>{{ post.id }}</span>
+                    <h2 class="post__title text-3xl text-center">{{
+                        post.title
+                    }}</h2>
+                    <p class="post__body text-center">{{ post.body }}</p>
+                    <div class="post__btns"> </div>
+                </div>
+            </NuxtLink>
+        </div>
     </div>
 </template>
 
---
+++ pages/posts/create.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="create p-10">
+        <h1 class="create__heading text-5xl mb-10">Create a new post</h1>
+        <form
+            class="create__form flex flex-col w-96"
+            action="#">
+            <input
+                class="border-cyan-800 border-2 rounded-xl p-2 mb-10"
+                type="text"
+                placeholder="post title" />
+            <textarea
+                class="border-cyan-800 border-2 rounded-xl p-2"
+                placeholder="post body" />
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
